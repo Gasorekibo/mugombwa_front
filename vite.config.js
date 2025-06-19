fd512ea04ee0b7c0155bd35b5eb4ae30b19bcba0
Gasorekibo <m.gasore@alustudent.com>
2025-06-19 17:57:33 +0200

    add production url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://mugombwa-back.onrender.com',
         changeOrigin: true,
       },
     },
